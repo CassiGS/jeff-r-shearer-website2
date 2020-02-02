1d2478d4595e9f8eebf0f260d03f3eca4cf18161
Cassi Gallagher-Shearer <cassi.gs@slalom.com>
2020-02-01 18:20:24 -0800

    Add bootstrap

--- wp-content/themes/jeff-shearer-blog/functions.php
+++ wp-content/themes/jeff-shearer-blog/functions.php
@@ -159,3 +159,11 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+function jeff_bootstrap_enqueue() {
+	wp_register_script( 'bootstrap-js', '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js', array('jquery'), NULL, true );
+	wp_register_style( 'bootstrap-css', '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css', false, NULL, 'all' );
+
+	wp_enqueue_script( 'bootstrap-js' );
+	wp_enqueue_style( 'bootstrap-css' );
+}
+add_action( 'wp_enqueue_scripts', 'jeff_bootstrap_enqueue' );
